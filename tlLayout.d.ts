2848091104f54709e3e1d7ff3b2a0171311765bc
Marco Bachmann <mb@triarc-labs.com>
2015-04-12 19:49:59 +0200

    Bumped version to 0.0.1

---
+++ tlLayout.d.ts
@@ -0,0 +1,9 @@
+declare module Triarc.Layout {
+    var mod: ng.IModule;
+}
+declare module Triarc.Layout {
+}
+declare module Triarc.Layout {
+    interface IWindowHeightScope extends ng.IScope {
+    }
+}
---
+++ tlLayout.js
@@ -0,0 +1,84 @@
+var Triarc;
+(function (Triarc) {
+    var Layout;
+    (function (Layout) {
+        Layout.mod = angular.module('tlLayout', []);
+    })(Layout = Triarc.Layout || (Triarc.Layout = {}));
+})(Triarc || (Triarc = {}));
+/// <reference path="tllayout.module.ts" />
+var Triarc;
+(function (Triarc) {
+    var Layout;
+    (function (Layout) {
+        Layout.mod.directive('tlHeightOf', ["$rootScope", function ($rootScope) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    element.css('overflow-y', 'auto');
+                    var owningClass = attrs.tlHeightOf;
+                    var owningElement = $(owningClass);
+                    var update = function () {
+                        var previousOverflow = owningElement.css("overflow-y");
+                        owningElement.css({ 'overflow': 'visible' });
+                        var height = owningElement[0].scrollHeight;
+                        if (attrs.tlHeightOfOffset) {
+                            height += +attrs.tlHeightOfOffset;
+                        }
+                        element.css('height', height);
+                        owningElement.css({ 'overflow': previousOverflow });
+                    };
+                    scope.$watch(function () { return owningElement[0].scrollHeight; }, function (val) {
+                        update();
+                    });
+                    attrs.$observe(attrs.tlHeightOf, function (newVal) {
+                        owningClass = attrs.tlHeightOf;
+                        owningElement = $(owningClass);
+                        update();
+                    });
+                    if (attrs.tlHeightOfOnEvent) {
+                        var eventName = scope.$eval(attrs.tlHeightOfOnEvent);
+                        scope.$on(eventName, function () {
+                            update();
+                        });
+                    }
+                }
+            };
+        }]);
+    })(Layout = Triarc.Layout || (Triarc.Layout = {}));
+})(Triarc || (Triarc = {}));
+/// <reference path="tllayout.module.ts" />
+// Update the most local relative references and declare this directive.
+var Triarc;
+(function (Triarc) {
+    var Layout;
+    (function (Layout) {
+        // declare correct angularjs module
+        Layout.mod.directive('tlWindowHeight', [
+            function () {
+                return {
+                    restrict: "A",
+                    link: function (scope, element, attrs) {
+                        var windowsListener = null;
+                        attrs.$observe("tlWindowHeight", function (offset) {
+                            var setHeight = function (offSetHeight) {
+                                var timeout = setTimeout(function () {
+                                    clearTimeout(timeout);
+                                    var containerHeight = element.closest(attrs["tlWindowHeightContainerClass"]).height();
+                                    element.height(containerHeight - +offSetHeight);
+                                }, 10);
+                            };
+                            if (angular.isString(offset) && attrs["tlWindowHeightContainerClass"]) {
+                                setHeight(offset);
+                                if (Triarc.hasNoValue(windowsListener)) {
+                                    windowsListener = $(window)[0].addEventListener("resize", function () {
+                                        setHeight(offset);
+                                    }, true);
+                                }
+                            }
+                        });
+                    }
+                };
+            }
+        ]);
+    })(Layout = Triarc.Layout || (Triarc.Layout = {}));
+})(Triarc || (Triarc = {}));
